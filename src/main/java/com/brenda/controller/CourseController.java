1c1c0f59e8db577de895f4fe2b79361a9c66196c
Brenda Souza <si.brendarenata@gmail.com>
2023-04-22 15:26:20 -0300

    feat: create course

--- src/main/java/com/brenda/controller/CourseController.java
+++ src/main/java/com/brenda/controller/CourseController.java
@@ -4,10 +4,9 @@ import java.util.List;
 
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.brenda.model.Course;
 import com.brenda.repository.CourseRepository;
@@ -25,6 +24,12 @@ public class CourseController {
     @GetMapping
     public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
     }
     
 }
